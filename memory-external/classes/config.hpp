30a083fbf437443adc135555efa3c5bbca0635ff
emrahcaliskan97 <emrahcaliskan33@outlook.com>
2025-10-17 09:56:54 +0300

    aİMBOT EKLENDİ.AMA BİRAZ GEÇ ALGILIYOR GİBİ BAKILACAK
    aİMBOT EKLENDİ.AMA BİRAZ GEÇ ALGILIYOR GİBİ BAKILACAK

--- memory-external/classes/config.hpp
+++ memory-external/classes/config.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <fstream>
+#include <windows.h>
 #include "utils.h"
 #include "json.hpp"
 #include "auto_updater.hpp"
@@ -7,35 +8,54 @@
 using json = nlohmann::json;
 
 struct RGB {
-	int r;
-	int g;
-	int b;
+    int r;
+    int g;
+    int b;
 
-	// Conversion function from RGB to COLORREF
-	operator COLORREF() const {
-		return RGB(r, g, b);
-	}
+    operator COLORREF() const {
+        return RGB(r, g, b);
+    }
 };
 
 namespace config {
-	const std::string file_path = "config.json";
+    // Exe yolunu alan fonksiyon
+    inline std::string get_exe_path() {
+        char buffer[MAX_PATH];
+        GetModuleFileNameA(NULL, buffer, MAX_PATH);
+        std::string::size_type pos = std::string(buffer).find_last_of("\\/");
+        return std::string(buffer).substr(0, pos);
+    }
 
-	extern bool read();
-	extern void save();
+    // Config dosya yolu - her çaðrýldýðýnda exe yolundan alýr
+    inline std::string get_config_path() {
+        return get_exe_path() + "\\config.json";
+    }
 
-	inline bool automatic_update = false;
-	inline bool team_esp = false;
-	inline float render_distance = -1.f;
-	inline int flag_render_distance = 200;
-	inline bool show_box_esp = true;
-	inline bool show_skeleton_esp = false;
-	inline bool show_head_tracker = false;
-	inline bool show_extra_flags = false;
+    extern bool read();
+    extern void save();
 
-	inline RGB esp_box_color_team = { 75, 175, 75 };
-	inline RGB esp_box_color_enemy = { 225, 75, 75 };
-	inline RGB esp_skeleton_color_team = { 75, 175, 75 };
-	inline RGB esp_skeleton_color_enemy = { 225, 75, 75 };
-	inline RGB esp_name_color = { 250, 250, 250 };
-	inline RGB esp_distance_color = { 75, 75, 175 };
+    inline bool automatic_update = false;
+    inline bool team_esp = false;
+    inline float render_distance = -1.f;
+    inline int flag_render_distance = 200;
+    inline bool show_box_esp = true;
+    inline bool show_skeleton_esp = false;
+    inline bool show_head_tracker = false;
+    inline bool show_extra_flags = false;
+
+    // Aimbot ayarlarý
+    inline bool aimbot_enabled = true;
+    inline float aimbot_smoothness = 0.1f;
+    inline float aimbot_max_distance = 2000000.0f;
+    inline bool aimbot_humanize = false;
+    inline bool aimbot_toggle_smooth = true;
+    inline float aimbot_aggressive_smoothness = 0.01f;
+    inline int aimbot_toggle_key = 112;
+
+    inline RGB esp_box_color_team = { 75, 175, 75 };
+    inline RGB esp_box_color_enemy = { 225, 75, 75 };
+    inline RGB esp_skeleton_color_team = { 75, 175, 75 };
+    inline RGB esp_skeleton_color_enemy = { 225, 75, 75 };
+    inline RGB esp_name_color = { 250, 250, 250 };
+    inline RGB esp_distance_color = { 75, 75, 175 };
 }
--- memory-external/hacks/reader.hpp
+++ memory-external/hacks/reader.hpp
@@ -66,7 +66,6 @@ public:
 	void loop();
 	void close();
 	Vector3 world_to_screen(Vector3* v);
-private:
 	view_matrix_t view_matrix;
 	uintptr_t entity_list;
 	uintptr_t localPlayer;
